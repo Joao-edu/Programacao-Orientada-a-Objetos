55b6f5d5b21b49532d091b5ca89ebf13ceff274a
Joao Eduardo <joaoeduardo616@yahoo.com>
2021-07-23 00:22:43 -0300

    Exercicio POO

--- exercicio_POO/Estoque_Ex2/Principal.java
+++ exercicio_POO/Estoque_Ex2/Principal.java
@@ -21,9 +21,29 @@ public class Principal {
 		System.out.println("Quantidade em estoque: ");
 		produto.quantidade = sc.nextInt();
 
-		System.out.println(produto);
-	
-	    sc.close();
+		System.out.println();
+		System.out.println("Informaçoes do produto: " + produto);
+		
+		System.out.println();
+		System.out.println("Entre com o numero de produto para ser adicionado no estoque: ");
+	    int quantidade = sc.nextInt();
+	    produto.addProduto(quantidade);
+	    
+	    System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+	   
+	    
+	    System.out.println();
+	    System.out.println("Entre com a quantidade de produtos a serem removidos do estoque");
+	    quantidade = sc.nextInt();
+	    produto.removerProduto(quantidade);    
+	    
+	    System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+	   
+		sc.close();
+	    
+	    
 	}
 
 }
